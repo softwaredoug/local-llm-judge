0534935ab1c077234922e9b5d02295128688301c
Doug Turnbull <doug@dahlialabs.ai>
2025-01-06 21:50:23 -0500

    Several stupid local llm as judge prompts

---
+++ eval_agent.py
@@ -0,0 +1,312 @@
+import phi_3_vision_mlx as pv
+
+
+def describe(query, product):
+
+    agent = pv.Agent(quantize_model=True, quantize_cache=True)
+
+    agent("Step by step, describe the information need behind the query: " + query)
+
+    product_prompt = f"""Here is the product:
+
+        Product Name: {product['name']}
+        Product Description: {product['description']}
+        Product Class: {product['class']}
+        Category Hierachy: {product['category_hierarchy']}
+
+        Describe the pros/cons/features of the product to satisfy the query: {query}
+
+    """
+
+    pros_cons = agent(product_prompt)
+    agent.end()
+    return pros_cons['responses']
+
+
+def parse_decision(response):
+    decision = response.split("\n")[-1]
+    if 'LHS' in decision and 'RHS' not in decision:
+        return 'LHS'
+    elif 'RHS' in decision and 'LHS' not in decision:
+        return 'RHS'
+    else:
+        return 'Neither'
+
+
+def decide(query, product_lhs, product_rhs, pros_lhs, pros_rhs):
+    instruction = [
+        "You are a text agent trying to be helpful when evaluating two products.",
+        f"Tell me which of the two products are most relevant based on the e-commerce search query: {query}"
+        "You will see the pros/cons/features of the two products and you are to decide which product is more relevant to the query." # noqa
+        f"Here is the first product, we'll name \"LHS\":\n\n {product_lhs['name']}",
+        f"Here is the second product, we'll name \"RHS\":\n\n {product_rhs['name']}",
+        f"Here are the pros/cons/features of the first product: {pros_lhs}",
+        f"Here are the pros/cons/features of the second product: {pros_rhs}",
+        f"Step by step, describe the information need behind the query: {query}",
+        f"Now step-by-step describe which product (LHS or RHS) is more relevant to the query: {query}"
+        f"Finally, conclude the last line with the product that is more relevant to the query.",
+        "IMPORTANT for parsing, in addition, finish with a single line with either simply 'LHS' or 'RHS' with your decision."
+    ]
+    response = pv.generate("\n\n".join(instruction))
+    decision = response.split("\n")[-1]
+    if 'LHS' in decision:
+        return 'LHS', response
+    elif 'RHS' in decision:
+        return 'RHS', response
+    else:
+        return 'Neither', response
+
+
+def describe_and_decide(query, product_lhs, product_rhs):
+    pros_lhs = describe(query, product_lhs)
+    pros_rhs = describe(query, product_rhs)
+    return decide(query, product_lhs, product_rhs, pros_lhs, pros_rhs)[0]
+
+
+def decide_just_title(query, product_lhs, product_rhs):
+    instruction = f"""
+        Which of these products is more relevant to the furniture e-commerce search query:
+
+        Query: {query}
+
+        Product LHS: {product_lhs['name']}
+        Propduct RHS: {product_rhs['name']}
+
+        Respond with just 'LHS' or 'RHS'
+    """
+    response = pv.generate(instruction)
+    return parse_decision(response)
+
+
+def decide_title_allow_neither(query, product_lhs, product_rhs):
+    if product_lhs['name'] == product_rhs['name']:
+        return 'Neither'
+    instruction = f"""
+        Neither product is more relevant to the query, unless given compelling evidence.
+
+        Which of these product names (if either) is more relevant to the furniture e-commerce search query:
+
+        Query: {query}
+
+        Product LHS name: {product_lhs['name']}
+            (remaining product attributes omited)
+        Or
+        Propduct RHS name: {product_rhs['name']}
+            (remaining product attributes omited)
+        Or
+        Neither / Need more product attributes
+
+        Only respond 'LHS' or 'RHS' if you are confident in your decision
+
+        Respond with just 'LHS - I am confident', 'RHS - I am confident', or 'Neither - not confident' with no other text. Respond 'Neither' if not enough evidence.
+    """
+    response = pv.generate(instruction)
+    return parse_decision(response)
+
+
+def decide_class_allow_neither(query, product_lhs, product_rhs):
+    if product_lhs['class'] == product_rhs['class']:
+        return 'Neither'
+
+    instruction = f"""
+        Neither product is more relevant to the query, unless given compelling evidence.
+
+        Which of these product names (if either) is more relevant to the furniture e-commerce search query:
+
+        Query: {query}
+
+        Product LHS class: {product_lhs['class']}
+            (remaining product attributes omited)
+        Or
+        Propduct RHS class: {product_rhs['class']}
+            (remaining product attributes omited)
+        Or
+        Neither / Need more product attributes
+
+        Only respond 'LHS' or 'RHS' if you are confident in your decision
+
+        Respond with just 'LHS - I am confident', 'RHS - I am confident', or 'Neither - not confident' with no other text. Respond 'Neither' if not enough evidence.
+    """
+    response = pv.generate(instruction)
+    return parse_decision(response)
+
+
+def decide_category_allow_neither(query, product_lhs, product_rhs):
+    if product_lhs['category_hierarchy'] == product_rhs['category_hierarchy']:
+        return 'Neither'
+
+    instruction = f"""
+        Neither product is more relevant to the query, unless given compelling evidence.
+
+        Which of these product names (if either) is more relevant to the furniture e-commerce search query:
+
+        Query: {query}
+
+        Product LHS category hierarchy: {product_lhs['category_hierarchy']}
+            (remaining product attributes omited)
+        Or
+        Propduct RHS category hierarchy: {product_rhs['category_hierarchy']}
+            (remaining product attributes omited)
+        Or
+        Neither / Need more product attributes
+
+        Only respond 'LHS' or 'RHS' if you are confident in your decision
+
+        Respond with just 'LHS - I am confident', 'RHS - I am confident', or 'Neither - not confident' with no other text. Respond 'Neither' if not enough evidence.
+    """
+    response = pv.generate(instruction)
+    return parse_decision(response)
+
+
+def decide_desc_allow_neither(query, product_lhs, product_rhs):
+    instruction = f"""
+        Neither product is more relevant to the query, unless given compelling evidence.
+
+        Which of these product names (if either) is more relevant to the furniture e-commerce search query:
+
+        Query: {query}
+
+        Product LHS description: {product_lhs['description']}
+            (remaining product attributes omited)
+        Or
+        Propduct RHS description: {product_rhs['description']}
+            (remaining product attributes omited)
+        Or
+        Neither / Need more product attributes
+
+        Only respond 'LHS' or 'RHS' if you are confident in your decision
+
+        Respond with just 'LHS - I am confident', 'RHS - I am confident', or 'Neither - not confident' with no other text. Respond 'Neither' if not enough evidence.
+    """
+    response = pv.generate(instruction)
+    return parse_decision(response)
+
+
+def decide_ensemble(query, product_lhs, product_rhs):
+    num_lhs = 0
+    num_rhs = 0
+    num_neither = 0
+    decision_fns = [decide_title_allow_neither, decide_class_allow_neither,
+                    decide_category_allow_neither, decide_desc_allow_neither]
+    for decision_fn in decision_fns:
+        decision = decision_fn(query, product_lhs, product_rhs)
+        if decision == 'LHS':
+            num_lhs += 1
+        elif decision == 'RHS':
+            num_rhs += 1
+        else:
+            return 'Neither'
+        # Present left as right, right as left
+        decision_reversed = decision_fn(query, product_rhs, product_lhs)
+        if decision_reversed == 'LHS':
+            num_rhs += 1
+        elif decision_reversed == 'RHS':
+            num_lhs += 1
+        else:
+            num_neither += 1
+        total_decisions = num_lhs + num_rhs + num_neither
+
+    print("Ensemble Complete")
+    print(f"num_lhs: {num_lhs}, num_rhs: {num_rhs}, num_neither: {num_neither}")
+
+    total_decisions = num_lhs + num_rhs + num_neither
+    if num_lhs == total_decisions:
+        return 'LHS'
+    elif num_rhs == total_decisions:
+        return 'RHS'
+    else:
+        return 'Neither'
+
+
+def decide_just_title_allow_neither2(query, product_lhs, product_rhs):
+    instruction = f"""
+        Youre playing a game. Youre evaluating the relevance of a product to a search query.
+
+        You get:
+           1  point for saying "I dont know"
+           2  points if you pick the product most relevant (LHS or RHS below)
+           -1 points for the wrong product name. So be careful! Choose LHS or RHS only when confident!
+
+        Which of these product names (if either) is more relevant to the furniture e-commerce search query:
+
+        Query: {query}
+
+        Product LHS name: {product_lhs['name']}
+            (remaining product attributes omited)
+        Or
+        Propduct RHS name: {product_rhs['name']}
+            (remaining product attributes omited)
+        Or
+        I dont know
+
+        Only respond 'LHS' or 'RHS' if you are confident in your decision
+
+        To play respond with just 'LHS', 'RHS', or 'I dont know' with no other text.
+    """
+    response = pv.generate(instruction)
+    return parse_decision(response)
+
+
+def decide_just_title_cot(query, product_lhs, product_rhs):
+    if not query:
+        raise ValueError("Query cannot be empty")
+    instruction = f"""
+        In one or two sentences, explain the information need behind this query: {query}.
+
+        Then decide which of these products is more relevant to the query
+
+        Product LHS: {product_lhs['name']}
+        Product RHS: {product_rhs['name']}
+
+        IMPORTANT: On a new line, your last line should just be 'LHS' or 'RHS',
+        indicating the most relevant product, with no other text for easier parsing.
+    """
+    response = pv.generate(instruction)
+    return parse_decision(response)
+
+
+def decide_just_title_cot2(query, product_lhs, product_rhs):
+    if not query:
+        raise ValueError("Query cannot be empty")
+    instruction = f"""
+        In one or two sentences, explain the information need behind this query: {query}.
+
+        In one or two sentences, describe each of these products characteristics:
+        Product LHS: {product_lhs['name']}
+        Product RHS: {product_rhs['name']}
+
+        Next, describe in a few sentences the pros/cons of these products for the query.
+
+        Then decide which of these products is more relevant to the query
+
+        IMPORTANT: On a new line, your last line should just be 'LHS' or 'RHS',
+        indicating the most relevant product, with no other text for easier parsing.
+    """
+    response = pv.generate(instruction)
+    return parse_decision(response)
+
+
+def decide_all_cot(query, product_lhs, product_rhs):
+    if not query:
+        raise ValueError("Query cannot be empty")
+    instruction = f"""
+        In one or two sentences, explain the information need behind this query: {query}.
+
+        In one or two sentences, describe each of these products characteristics:
+        Product LHS: {product_lhs['name']}
+           Description: {product_lhs['description']}
+                 Class: {product_lhs['class']}
+    Category Hierarchy: {product_lhs['category_hierarchy']}
+        Product RHS: {product_rhs['name']}
+           Description: {product_rhs['description']}
+                 Class: {product_rhs['class']}
+    Category Hierarchy: {product_rhs['category_hierarchy']}
+
+
+        Next, describe in a few sentences the pros/cons of these products for the query.
+
+        Then decide which of these products is more relevant to the query by simply
+        placing 'LHS' or 'RHS' on the last line.
+    """
+    response = pv.generate(instruction)
+    return parse_decision(response)
---
+++ pairwise.py
@@ -0,0 +1,138 @@
+import pandas as pd
+import eval_agent
+
+
+def pairwise_df(n=250):
+    products = pd.read_csv('data/WANDS/dataset/product.csv', delimiter='\t')
+    queries = pd.read_csv('data/WANDS/dataset/query.csv', delimiter='\t')
+    labels = pd.read_csv('data/WANDS/dataset/label.csv', delimiter='\t')
+    labels.loc[labels['label'] == 'Exact', 'grade'] = 2
+    labels.loc[labels['label'] == 'Partial', 'grade'] = 1
+    labels.loc[labels['label'] == 'Irrelevant', 'grade'] = 0
+    labels = labels.merge(queries, how='left', on='query_id')
+    labels = labels.merge(products, how='left', on='product_id')
+
+    # Sample n rows
+    labels = labels.sample(n * 10, random_state=42)
+
+    # Get pairwise
+    pairwise = labels.merge(labels, on='query_id')
+
+    # Drop same id
+    pairwise = pairwise[pairwise['product_id_x'] != pairwise['product_id_y']]
+
+    # Drop same rating
+    pairwise = pairwise[pairwise['label_x'] != pairwise['label_y']]
+
+    return pairwise.sample(n, random_state=42)
+
+
+def product_row_to_dict(row):
+    if 'product_name_x' in row:
+        return {
+            'id': row['product_id_x'],
+            'name': row['product_name_x'],
+            'description': row['product_description_x'],
+            'class': row['product_class_x'],
+            'category_hierarchy': row['category hierarchy_x'],
+            'grade': row['grade_x']
+        }
+    elif 'product_name_y' in row:
+        return {
+            'id': row['product_id_y'],
+            'name': row['product_name_y'],
+            'description': row['product_description_y'],
+            'class': row['product_class_y'],
+            'category_hierarchy': row['category hierarchy_y'],
+            'grade': row['grade_y']
+        }
+
+
+def output_row(query, product_lhs, product_rhs, human_preference, agent_preference):
+    return {
+        'query': query,
+        'product_name_lhs': product_lhs['name'],
+        'product_description_lhs': product_lhs['description'],
+        'product_id_lhs': product_lhs['id'],
+        'product_class_lhs': product_lhs['class'],
+        'category_hierarchy_lhs': product_lhs['category_hierarchy'],
+        'grade_lhs': product_lhs['grade'],
+        'product_name_rhs': product_rhs['name'],
+        'product_description_rhs': product_rhs['description'],
+        'product_id_rhs': product_rhs['id'],
+        'product_class_rhs': product_rhs['class'],
+        'category_hierarchy_rhs': product_rhs['category_hierarchy'],
+        'grade_rhs': product_rhs['grade'],
+        'human_preference': human_preference,
+        'agent_preference': agent_preference
+    }
+
+
+def eval(eval_fn=eval_agent.decide_ensemble, assert_on_diff=True):
+    df = pairwise_df()
+    same_preference = 0
+    different_preference = 0
+    func_name = eval_fn.__name__
+    results_df = pd.DataFrame()
+    try:
+        results_df = pd.read_pickle(f'data/{func_name}.pkl')
+        same_preference = len(results_df[results_df['human_preference'] == results_df['agent_preference']])
+        different_preference = len(results_df[results_df['human_preference'] != results_df['agent_preference']])
+    except FileNotFoundError:
+        pass
+    for idx, row in df.iterrows():
+        query = row['query_x']
+        product_lhs = product_row_to_dict(row[['product_name_x', 'product_description_x',
+                                               'product_class_x',
+                                               'product_id_x', 'category hierarchy_x', 'grade_x']])
+        product_rhs = product_row_to_dict(row[['product_name_y', 'product_description_y',
+                                               'product_class_y',
+                                               'product_id_y', 'category hierarchy_y', 'grade_y']])
+        result_exists = len(results_df) > 0 and (results_df[(results_df['query'] == query) &
+                                                            (results_df['product_id_lhs'] == product_lhs['id']) &
+                                                            (results_df['product_id_rhs'] == product_rhs['id'])].shape[0] > 0)
+        if result_exists:
+            print(f"Already rated query: {query}, product_lhs: {product_lhs['name']}, product_rhs: {product_rhs['name']}")
+            print("Skipping")
+            continue
+        human_preference = product_lhs['grade'] - product_rhs['grade']
+        if human_preference > 0:
+            human_preference = 'LHS'
+        elif human_preference < 0:
+            human_preference = 'RHS'
+        else:
+            human_preference = 'Neither'
+
+        agent_preference = eval_fn(query, product_lhs, product_rhs)
+        print(f"Grade LHS: {product_lhs['grade']}, Grade RHS: {product_rhs['grade']}")
+        print(f"Human Preference: {human_preference}, Agent Preference: {agent_preference}")
+
+        results_df = pd.concat([results_df, pd.DataFrame([output_row(query, product_lhs, product_rhs, human_preference,
+                                                                     agent_preference)])])
+        same_preference = len(results_df[results_df['human_preference'] == results_df['agent_preference']]) if (
+                len(results_df) > 0) else 0
+        no_preference = len(results_df[results_df['agent_preference'] == 'Neither']) if (len(results_df) > 0) else 0
+        different_preference = len(results_df[(results_df['human_preference'] != results_df['agent_preference']) & (
+                results_df['human_preference'] != 'Neither') & (results_df['agent_preference'] != 'Neither')]) if (
+                len(results_df) > 0) else 0
+        print(f"Same Preference: {same_preference}, Different Preference: {different_preference}, No Preference: {no_preference}")
+        if agent_preference != human_preference and agent_preference != 'Neither' and human_preference != 'Neither':
+            print("DIFFERENT PREFERENCE")
+        if (same_preference + different_preference) > 0:
+            print(f"Percentage same preference: {same_preference / (same_preference + different_preference) * 100}%")
+
+        results_df.to_pickle(f'data/{func_name}.pkl')
+    same_preference = len(results_df[results_df['human_preference'] == results_df['agent_preference']]) if (
+            len(results_df) > 0) else 0
+    no_preference = len(results_df[results_df['agent_preference'] == 'Neither']) if (len(results_df) > 0) else 0
+    different_preference = len(results_df[(results_df['human_preference'] != results_df['agent_preference']) & (
+            results_df['human_preference'] != 'Neither') & (results_df['agent_preference'] != 'Neither')]) if (
+            len(results_df) > 0) else 0
+    print(f"Same Preference: {same_preference}, Different Preference: {different_preference}, No Preference: {no_preference}")
+    if (same_preference + different_preference) > 0:
+        print(f"Percentage same preference: {same_preference / (same_preference + different_preference) * 100}%")
+
+
+
+if __name__ == '__main__':
+    eval()
